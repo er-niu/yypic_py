f66143682464e8ac455916470a44ac402564ca96
er-niu <niuvip6688@163.com>
2019-01-02 22:34:56 +0800

    system config

--- yypic_py/conf/read_conf.py
+++ yypic_py/conf/read_conf.py
@@ -4,9 +4,8 @@ import ConfigParser
 
 conf = ConfigParser.SafeConfigParser()
 print '################### read system.cfg'
-# conf.read("/usr/local/src/yywallpaper-py/dist/yywallpaper-py-0.1/system.cfg")
-conf.read("../system.cfg")
-
+conf.read("/usr/local/src/yypic_py/dist/yypic_py-0.1/system.cfg")
+# conf.read("../system.cfg")
 
 def get_conf(section, key):
     return conf.get(section, key)
